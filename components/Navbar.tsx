d86951ca74155c63379bc91d27b76a006db2087f
shm_usuf <shmusuf@gmail.com>
2023-07-16 16:28:18 +0600

    Navbar desgin added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,45 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <header className="h-20 fixed top-0 left-0 right-0 bg-rose-500">
-      Navbar
+    <header className="h-20 fixed top-0 left-0 right-0 bg-light/90 backdrop-blur-lg border-b border-dark/10 z-[100]">
+      <div className="wrapper w-full h-full flex justify-between items-center">
+        {/* NAV LEFT */}
+        <nav>
+          <Link href="/" className="link-item text-lg font-medium">
+            Sparkle Mart
+          </Link>
+        </nav>
+        {/* NAV MID */}
+        <nav>
+          <ul className="flex gap-5">
+            <li>
+              <Link href="/" className="link-item">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/products" className="link-item">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="link-item">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="link-item">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/* NAV RIGHT */}
+        <nav>
+          <Link href="/login">Login</Link>
+        </nav>
+      </div>
     </header>
   );
 };
