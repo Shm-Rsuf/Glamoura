12a485d05584796529e5e10dc305371de5cc3e5e
shm_usuf <shmusuf@gmail.com>
2023-08-19 23:27:51 +0600

    header structure added

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,5 +1,48 @@
+import Link from "next/link";
+
 const Navbar = () => {
-  return <header>Navbar</header>;
+  return (
+    <header>
+      <div className="">
+        {/* LEFT NAV */}
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Link 1</Link>
+            </li>
+            <li>
+              <Link href="/">Link 2</Link>
+            </li>
+            <li>
+              <Link href="/">Link 3</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* MID NAV */}
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Sparkle Mart</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* RIGHT NAV */}
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Link 4</Link>
+            </li>
+            <li>
+              <Link href="/">Link 5</Link>
+            </li>
+            <li>
+              <Link href="/user/signin">Sign in</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
