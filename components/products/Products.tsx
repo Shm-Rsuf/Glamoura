16850e6dc981ea9976f4b689bc2b36f2fbbd1baf
shm_usuf <shmusuf@gmail.com>
2023-09-04 21:39:52 +0600

    products data added

--- components/products/Products.tsx
+++ components/products/Products.tsx
@@ -4,6 +4,10 @@ const Products = () => {
   return (
     <section className='wrapper section-padding' id='products'>
       <SectionTitle title='Products' subtitle='Explore our beauty products' />
+      {/* Products */}
+      <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
+        {/* Multiple products goes here */}
+      </div>
     </section>
   );
 };
