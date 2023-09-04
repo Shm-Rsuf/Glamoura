0190a041c29b368641956069ffc1312e0edfd943
shm_usuf <shmusuf@gmail.com>
2023-09-04 21:01:32 +0600

    products component added

---
+++ components/products/Products.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from '../shared/SectionTitle';
+
+const Products = () => {
+  return (
+    <section className='wrapper section-padding' id='products'>
+      <SectionTitle title='Products' subtitle='Explore our beauty products' />
+    </section>
+  );
+};
+
+export default Products;
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -16,8 +16,8 @@ const Navbar = async () => {
               </Link>
             </li>
             <li>
-              <Link href='/' className='link-item'>
-                Link2
+              <Link href='/#products' className='link-item'>
+                Products
               </Link>
             </li>
             <li>
