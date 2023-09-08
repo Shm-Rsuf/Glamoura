00b7e3cdcac03417615422b91d97f63ea2fa030b
shm_usuf <shmusuf@gmail.com>
2023-09-08 21:41:34 +0600

    gallery section added

--- components/home/Gallery.tsx
+++ components/home/Gallery.tsx
@@ -1,14 +1,51 @@
+import { data } from '@/data/gallery';
 import SectionTitle from '../shared/SectionTitle';
+import { cn } from '@/library/utils';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Gallery = () => {
   return (
-    <section className='my-20 h-screen bg-dark text-light'>
+    <section className='my-20 min-h-screen bg-dark text-light'>
       <div className='wrapper section-padding h-full w-full'>
         <SectionTitle
           title='Gallery'
           subtitle='Explore our gallery section'
           color='bg-orange'
         />
+
+        {/* grid parent */}
+        <div className='gallery-grid_parent relative h-screen w-full gap-10'>
+          {data.map((item: any) => (
+            <div
+              key={item.id}
+              className={cn(
+                item.class,
+                'h-full w-full overflow-hidden border-8'
+              )}
+            >
+              <div className='h-full w-full overflow-hidden bg-dark'>
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  width={360}
+                  height={360}
+                  priority
+                  className='h-full w-full object-cover'
+                />
+              </div>
+            </div>
+          ))}
+
+          <div className='eq group absolute bottom-20 left-[50%] top-[50%] z-[1] flex h-36 w-36 -translate-x-1/2 -translate-y-1/2 items-center justify-center overflow-hidden rounded-full bg-blue/70 text-lg uppercase text-light shadow-lg shadow-dark/50 backdrop-blur-md group-hover:shadow-blue/50'>
+            <p className='z-[3]'>Gallery</p>
+            {/* filler 1*/}
+            <span className='eq absolute bottom-0 left-0 right-0 top-full z-[2] h-1/2 w-full bg-dark/70 group-hover:top-1/2'></span>
+
+            {/* filler 2*/}
+            <span className='eq absolute -top-full bottom-0 left-0 right-0 z-[2] h-1/2 w-full bg-dark/70 group-hover:top-0'></span>
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ data/gallery.ts
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    id: 1,
+    class: 'grid-child-1',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 2,
+    class: 'grid-child-2',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 3,
+    class: 'grid-child-3',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 4,
+    class: 'grid-child-4',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 5,
+    class: 'grid-child-5',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 6,
+    class: 'grid-child-6',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 7,
+    class: 'grid-child-7',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 8,
+    class: 'grid-child-8',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 9,
+    class: 'grid-child-9',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+  {
+    id: 10,
+    class: 'grid-child-10',
+    src: 'https://images.pexels.com/photos/2383117/pexels-photo-2383117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'women',
+  },
+];
